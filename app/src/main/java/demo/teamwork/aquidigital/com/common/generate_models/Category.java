e0454ec877794030054e1f851565d30abfbe7a2a
Ruairi McGuigan <r.mcguigan@waracle.com>
2018-05-08 15:26:45 +0100

    models added

---
+++ app/src/main/java/demo/teamwork/aquidigital/com/common/generate_models/Category.java
@@ -0,0 +1,23 @@
+package demo.teamwork.aquidigital.com.common.generate_models;
+
+import com.google.auto.value.AutoValue;
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.Gson;
+
+@AutoValue
+public abstract class Category{
+
+	@SerializedName("id")
+	public abstract String id();
+
+	@SerializedName("name")
+	public abstract String name();
+
+	@SerializedName("color")
+	public abstract String color();
+
+	public static TypeAdapter<Category> typeAdapter(Gson gson) {
+		return new AutoValue_Category.GsonTypeAdapter(gson);
+	}
+}
